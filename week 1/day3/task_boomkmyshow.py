5816099775ac303624c0b48a5a9779bb8855bfee
Gurassis Kaur <g.assiskaur@gmail.com>
2026-06-29 00:29:56 +0530

    week 1

---
+++ week 1/day3/task_boomkmyshow.py
@@ -0,0 +1,96 @@
+tickets = {
+    'A' : [
+        {
+            'seat_no': 1,
+            'avalaibility':'avaliable',
+            'cateogry':'classic',
+            'type':'normal',
+            'price':300
+        },
+        {
+            'seat_no': 2,
+            'avalaibility':'avaliable',
+            'cateogry':'classic',
+            'type':'normal',
+            'price':300
+        }
+        #upto seat 13
+    ],
+
+
+    'B' : [
+        {
+            'seat_no': 1,
+            'avalaibility':'avaliable',
+            'cateogry':'classic',
+            'type':'normal',
+            'price':300
+        },
+        {
+            'seat_no': 2,
+            'avalaibility':'avaliable',
+            'cateogry':'classic',
+            'type':'normal',
+            'price':300
+        }
+        
+        #upto seat 13
+    ],
+    #similary row C 
+
+    'D' : [
+        {
+            'seat_no': 1,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'normal',
+            'price':380
+        },
+        {
+            'seat_no': 2,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'normal',
+            'price':380
+        }
+        #upto row 13
+
+    ], 
+
+    'E': [
+        {
+            'seat_no': 1,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'normal',
+            'price':380
+        },
+        {
+            'seat_no': 2,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'normal',
+            'price':380
+        }
+        #upto row 11
+    ],
+
+    'F' : [
+        {
+            'seat_no': 1,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'Special_needs',
+            'price':380
+        },
+        {
+            'seat_no': 2,
+            'avalaibility':'avaliable',
+            'cateogry':'prime',
+            'type':'special needs',
+            'price':380
+        }
+        #upto row 14
+
+    ]
+}
---
+++ week 1/day3/task_makemytrip.py
@@ -0,0 +1,33 @@
+Flights = [
+    {
+        'airline': 'Air India',
+        'airplane': 'AI 7713',
+
+        'From': 'New Delhi',
+        'To': 'zurich',
+
+        'Departure': '2026-06-30 1:05',
+        'Arrival': '2026-06-30 6:20',
+        'Duration': '8h 45m',
+
+        'Price': 50000,
+        'stops': 0
+ 
+    },
+    {
+        'airline': 'Swiss',
+        'airplane': 'LX 2646',
+
+        'From': 'Zurich',
+        'To': 'zNew Delhi',
+
+        'Departure': '2026-07-15 12:35',
+        'Arrival': '2026-07-16 00:05',
+        'Duration': '8h',
+
+        'Price': 50000,
+        'stops': 0
+ 
+    },
+
+]
