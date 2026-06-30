0a371ee3921b4bc13aba6cbf638a897058988697
banee <baneetejas2089@gmail.com>
2026-07-01 00:47:56 +0530

    day 2

--- week 1/day2/1.py
+++ week 1/day2/1.py
@@ -1,2 +1,9 @@
-age = input('enter age: ')
-print ('age is : ', age , id(age), type(age))
+#discussing single valued containers and representation in memory
+
+user_name= 'g.assis'
+age = 20
+eves_age = 20
+
+print('Hashcode of user_name:', id(user_name))
+print('Hashcode of age:', id(age))
+print('Hashcode of eves_age:', id(eves_age))
---
+++ week 2/day 5/zomatopromocodes.py
@@ -0,0 +1,50 @@
+promo_codes = {
+    'BINGO': {
+        'minimum_amount': 200,
+        'discount': 0.50,
+        'discount_upto': 200
+    },
+    'JUMBO': {
+        'minimum_amount': 500,
+        'discount': 0.30,
+        'discount_upto': 0
+    }
+}
+
+amount_in_cart = int(input('Enter Amount: '))
+promo_code = input('Enter Promo Code: ')
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('You Entered Amount:', amount_in_cart)
+print('You Entered Promo Code:', promo_code)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+if promo_code in promo_codes:
+    print('Promo Code Valid', promo_code)
+    promo_code_rule = promo_codes[promo_code]
+    print(promo_code_rule, type(promo_code_rule))
+
+    if promo_code_rule['discount_upto'] > 0:
+
+        if amount_in_cart > promo_code_rule['minimum_amount']:
+            discount_calculated = amount_in_cart - (promo_code_rule['discount']*amount_in_cart) #computational statemenyts deleted once if is executed. 
+            print('discount_calculated: \u20b9', discount_calculated)
+
+            if discount_calculated > promo_code_rule['discount_upto']:
+                amount_to_pay = amount_in_cart - promo_code_rule['discount_upto']
+            else:
+                amount_to_pay = amount_in_cart - discount_calculated
+
+            print('Promo Code applied successfully')
+            print('Please Pay: \u20b9', amount_to_pay)
+
+        else:
+            print('Promo Code cannot be applied. Enter items worth',  promo_code_rule['minimum_amount'] - amount_in_cart, 'more')
+    else:
+        if amount_in_cart > promo_code_rule['minimum_amount']:
+            discount_calculated = amount_in_cart - (promo_code_rule['discount']*amount_in_cart)
+            amount_to_pay = discount_calculated
+
+
+else:
+    print('Promo Code InValid')
