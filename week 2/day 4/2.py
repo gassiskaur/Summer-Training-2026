1eda2a768cc811f714328d020ab6c1cf3a88b798
Gurassis Kaur <g.assiskaur@gmail.com>
2026-07-01 19:51:52 +0530

    day 4

---
+++ week 2/day 4/2.py
@@ -0,0 +1,31 @@
+# mainframe in memory:
+
+a = 10
+b = {10, 20, 30}
+
+#basic of loops:
+
+#SET
+data=[10,20,30,40,50]
+for number in data:
+    print(number)
+
+
+for index in range(0,5):
+    print(index)
+
+#for loop for dictionary:
+
+numbers = {1: "one", 2: "two", 3: "three"} 
+for key in numbers:
+    print (key, numbers[key]) 
+
+
+# WHILE LOOP:
+
+i=0
+while i<5:
+    print(i)
+    i+=1
+
+    
