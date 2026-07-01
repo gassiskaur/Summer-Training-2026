94673439b49744357a259d94d203b3688292b7a7
banee <baneetejas2089@gmail.com>
2026-07-01 15:52:04 +0530

    day 3

--- week 1/day3/1.py
+++ week 1/day3/1.py
@@ -1,3 +1,5 @@
+#refrence copy operation 
+
 a=10
 b=a
 
@@ -5,8 +7,8 @@ print("ID of a: ", id(a))
 print("ID of b: ", id(b))
 
 b=100
-print("b: ", b, id(b))
-print("a: ",a, id(a))
+print("b: ", b, 'ID of b: ', id(b))
+print("a: ",a, 'ID of a: ', id(a))
 
 data=[10,20,30,40,50]
 numbers=data
@@ -17,9 +19,6 @@ print("id of numbers: ", id(numbers))
 data[1]=1000
 print('data[1]: ', data[1])
 print('numbers[1]: ', numbers[1])
-print("id of a: ", id(a))
-print("id of b: ", id(b))
-
 
 numbers[3]=4000
 print('numbers[3]: ',numbers[3] )
