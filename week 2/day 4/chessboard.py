de59a7657696993647f9da2557c97fbfba2c1e59
banee <baneetejas2089@gmail.com>
2026-07-02 18:58:47 +0530

    day 5

--- week 2/day 4/chessboard.py
+++ week 2/day 4/chessboard.py
@@ -1,24 +1,29 @@
+wpawn = '\u2659'
+bpawn = '\u265F'
 
-wpawn='\u2659'
-bpawn='\u265F'
+wpieces = ['\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657', '\u2658', '\u2656']
+bpieces = ['\u265C', '\u265E', '\u265D', '\u265B', '\u265A', '\u265D', '\u265E', '\u265C']
 
 for outer_index in range(8):
 
-
     for inner_index in range(8):
 
+        if outer_index == 0:
+            print(wpieces[inner_index], end=" ")
 
-        if outer_index== 1 :
+        elif outer_index == 1:
             print(wpawn, end=" ")
- 
-        elif outer_index== 6 :
+
+        elif outer_index == 6:
             print(bpawn, end=" ")
-      
-        else :
-            if (outer_index+ inner_index)% 2 ==0:
-                print ("\u25A0", end=" ")
-            else :
+
+        elif outer_index == 7:
+            print(bpieces[inner_index], end=" ")
+
+        else:
+            if (outer_index + inner_index) % 2 == 0:
+                print("\u25A0", end=" ")
+            else:
                 print("\u25A1", end=" ")
 
-    
     print()
